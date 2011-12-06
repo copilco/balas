9dd87be6a2d19ea55ea9a338e94b687fe2d5252b
copilco1 <copilco1@d2f79fa8-e154-11dd-b2d9-215335d0b316>
2011-12-06 19:11:44 +0000

    New Object for 2D Uniform

--- HankelFFTIntel/include/tools.h
+++ HankelFFTIntel/include/tools.h
@@ -48,7 +48,77 @@ void tridag(complex *a,complex *b,complex *c,complex *r,complex *u, int n,comple
 }
 
 
+void tridagS(complex a,complex *b,complex c,complex *r,complex *u, int n,complex *gam1)
+{
+	
+	complex bet=b[0]; // Declare and define auxiliar array
+	
+	if(b[0]==0.0) nrerror ("error 1 en tridag");
+	u[0]=r[0]/bet;
+	
+	for(int j=1;j<n;j++)
+	{
+		gam1[j] = c/bet;
+		bet     = b[j] - a*gam1[j];
+		
+		if (bet==0.0) nrerror("error 2 en tridag");
+		u[j]  = (r[j]-a*u[j-1])/bet;
+	}
+	
+	for(int j=(n-2);j>=0;j--)
+		u[j]-=gam1[j+1]*u[j+1];
+	
+}
 
+
+/* New tridag made by Alexis Used in the 2D Uniform*/
+
+//void trid_simple( complex a, vector<complex> &b, complex c, vector<complex> &r, vector<complex> &u,vector<complex> &gamz , int n)
+void trid_simple( complex a, complex *b, complex c, complex *r, complex *u,complex *gamz , int n)
+{
+	int j;
+	//gam1.resize( n, 0. );
+	for(j=0;j<n;j++)
+		gamz[0] = complex(0.,0.);
+	
+	complex bet=b[0];
+	u[0]=r[0]/bet;
+	
+	for(j=1;j<n;j++)
+	{
+		gamz[j] = c/bet;
+		bet     = b[j] - a*gamz[j];
+		
+		
+		u[j]  = (r[j]-a*u[j-1])/bet;
+	}
+	
+	for(j=(n-2);j>=0;j--)
+		u[j]-= gamz[j+1]*u[j+1];
+	
+}
+
+
+void tridagAlexis( vector<complex> &a, vector<complex> &b, vector<complex> &c, vector<complex> &r, vector<complex> &u, vector<complex> &gamr,int n)
+{
+	int j;
+//	gam1.resize( n, 0. );
+	
+	complex bet = b[0];
+	u[0]=r[0]/bet;
+	
+	for(j=1;j<n;j++)
+	{
+		gamr[j] = c[j-1]/bet;
+		bet     = b[j] - a[j]*gamr[j];
+		
+		u[j]  = (r[j]-a[j]*u[j-1])/bet;
+	}
+	
+	for(j=(n-2);j>=0;j--)
+		u[j]-=gamr[j+1]*u[j+1];
+	
+}
 
 
 
